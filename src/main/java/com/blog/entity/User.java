2c9b458dd7fdb5d7ac1118dfdab17e051e27ceb9
马通 <907674285@qq.com>
2020-02-21 16:01:54 +0800

    start

---
+++ src/main/java/com/blog/entity/User.java
@@ -0,0 +1,100 @@
+package com.blog.entity;
+
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Long id;
+
+    private String nickName;
+
+    private String email;
+
+    private String phone;
+
+    private String imgUrl;
+
+    private String password;
+
+    private String state;
+
+    private String enable;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName == null ? null : nickName.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl == null ? null : imgUrl.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+
+    public String getEnable() {
+        return enable;
+    }
+
+    public void setEnable(String enable) {
+        this.enable = enable == null ? null : enable.trim();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nickName='" + nickName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", imgUrl='" + imgUrl + '\'' +
+                ", password='" + password + '\'' +
+                ", state='" + state + '\'' +
+                ", enable='" + enable + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/mybatisTest.java
@@ -0,0 +1,55 @@
+import com.blog.entity.User;
+import com.blog.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class mybatisTest {
+    /**
+     * 测试查询所有
+     */
+    @Test
+    public void testFindAll() throws Exception{
+//       1. 读取配置文件，生成字节输入流
+        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+//        2.获取sqlsessionFactory
+        SqlSessionFactory factory = new  SqlSessionFactoryBuilder().build(in);
+//        3.获取 SqlSession对象
+        SqlSession sqlSession = factory.openSession();
+//        4.获取dao的代理对象
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//        5执行查询所有方法
+        List<User> users = userMapper.findAll();
+        for (User user :users){
+            System.out.println(user);
+        }
+        sqlSession.close();
+        in.close();
+    }
+
+
+    @Test
+    public void testInsert() throws Exception{
+//        1. 读取配置文件，生成字节输入流
+        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+//        2.获取sqlsessionFactory
+        SqlSessionFactory factory = new  SqlSessionFactoryBuilder().build(in);
+//        3.获取 SqlSession对象
+        SqlSession sqlSession = factory.openSession();
+//        4.获取dao的代理对象
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+//        5执行insert方法
+        User user = new User();
+        user.setNickName("你大爷的");
+        user.setEmail("1111@qq.com");
+        user.setPhone("10086");
+        userMapper.insert(user);
+        sqlSession.commit();
+    }
+
+}
